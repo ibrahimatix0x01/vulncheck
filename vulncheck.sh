5bbaa2f7c77aaa1c8b935cd21544bbb49e1cfa4d
ibrahimatix0x01 <96072328+ibrahimatix0x01@users.noreply.github.com>
2022-04-23 11:06:07 +0100

    Update vulncheck.sh

--- vulncheck.sh
+++ vulncheck.sh
@@ -54,8 +54,8 @@ cat $OUT_DIR/$TARGET/vulncheck.txt | httprobe > $OUT_DIR/$TARGET/probed.txt
 echo "[+] Live subdomains are saved to: $OUT_DIR/$TARGET/probed.txt"
 
 
-nuclei -list $OUT_DIR/$TARGET/probed.txt -severity low,medium,high, critical -o $OUT_DIR/$TARGET/final.txt
-echo "[+] Final results are saved to: $OUT_DIR/$TARGET/final.txt" 
+nuclei -list $OUT_DIR/$TARGET/probed.txt -o $OUT_DIR/$TARGET/final.txt
+echo "[+] Final results are saved to: $OUT_DIR/$TARGET/final.txt"
 
 NUCRES=$(cat $OUT_DIR/$TARGET/final.txt | wc -l)
 
